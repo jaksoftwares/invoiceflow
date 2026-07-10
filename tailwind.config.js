706500c992941360fd822382a1dabb0882812e8b
Jaksoftwares <jaksoftwares05@gmail.com>
2026-07-10 19:18:04 +0300

    chore: increase marquee animation duration to 300s

--- tailwind.config.js
+++ tailwind.config.js
@@ -67,7 +67,7 @@ module.exports = {
         },
       },
       animation: {
-        marquee: 'marquee 120s linear infinite',
+        marquee: 'marquee 300s linear infinite',
       },
     },
   },
